d3871b6da85dbaa39fc33fcdd214c79cf1996059
AlexandreMercierAubin <alexandremercieraubin@gmail.com>
2017-09-14 17:28:21 -0400

    creating dev branch

--- Gaudrophone/src/gaudrophone/Gaudrophone.java
+++ Gaudrophone/src/gaudrophone/Gaudrophone.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**********************************************
+ * Gaudrophone
+ * 
+ * Description:
+ * 
+ * 9/14/2017
+ * Alexandre Mercier-Aubin
+ * Samuel Baillargeon
+ * Alexandre Venables
+ * Emile Gelinas
+ **********************************************
  */
 package gaudrophone;
 
-/**
- *
- * @author AlexLaptop
- */
 public class Gaudrophone {
 
     /**
