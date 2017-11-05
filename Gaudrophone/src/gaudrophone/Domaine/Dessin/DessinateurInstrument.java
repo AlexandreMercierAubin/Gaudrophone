4a6d1e59d64a4ee97e4d503b951e1b5d8a0e2099
Samuel Baillargeon <samuel.baillargeon.1@ulaval.ca>
2017-11-05 09:59:05 -0500

    Refactoring du dessinateur.

--- Gaudrophone/src/gaudrophone/Domaine/Dessin/DessinateurInstrument.java
+++ Gaudrophone/src/gaudrophone/Domaine/Dessin/DessinateurInstrument.java
@@ -2,7 +2,6 @@ package gaudrophone.Domaine.Dessin;
 
 import gaudrophone.Domaine.ControleurInstrument;
 import gaudrophone.Domaine.Dimension2D;
-import gaudrophone.Domaine.Enums.Forme;
 import gaudrophone.Domaine.Instrument.*;
 import gaudrophone.Domaine.Outils;
 import java.awt.BasicStroke;
@@ -29,117 +28,144 @@ public class DessinateurInstrument {
         List<Touche> touches = controleur.getInstrument().getTouches();
         
         for (Touche touche : touches)
-        {
-            ApparenceTouche apparence = touche.getApparence();
-            Point2D position = Outils.conversionPointRelatifPixel(touche.getPosition(), dimensionPanneau);
-            Dimension2D dimension = Outils.conversionDimensionRelatifPixel(apparence.getDimension(), dimensionPanneau);
-            Forme forme = apparence.getForme();
-            
-            int x, y;
-            Bordure bordure;
-            
-            switch (forme)
+        {   
+            switch (touche.getApparence().getForme())
             {
                 case Cercle:
-                    // Dessin du cercle intérieur
-                    g2.setColor(apparence.getCouleurFond());
-                    x = (int)position.getX() - (int)dimension.getWidth() / 2;
-                    y = (int)position.getY() - (int)dimension.getHeight() / 2;
-                    g2.fillOval(x, y, (int)dimension.getWidth(), (int)dimension.getHeight());
-                    
-                    // Dessin de la bordure
-                    bordure = apparence.getBordure(0);
-                    if (bordure != null)
-                    {
-                        g2.setColor(bordure.getCouleur());
-                        g2.setStroke(new BasicStroke(bordure.getLargeur()));
-                        g2.drawOval(x, y, (int)dimension.getWidth(), (int)dimension.getHeight());
-                    }
+                    dessinerCercle(touche, g2);
                     break;
                     
                 case Rectangle:
-                    // Dessin du rectangle intérieur
-                    g2.setColor(apparence.getCouleurFond());
-                    x = (int)position.getX() - (int)dimension.getWidth() / 2;
-                    y = (int)position.getY() - (int)dimension.getHeight() / 2;
-                    Rectangle rectangle = new Rectangle(x, y, (int)dimension.getWidth(), (int)dimension.getHeight());
-                    g2.fill(rectangle);
-                    
-                    // Dessin des bordures
-                    for (int i = 0; i < 4; i++)
-                    {
-                        bordure = apparence.getBordure(i);
-                        g2.setColor(bordure.getCouleur());
-                        g2.setStroke(new BasicStroke(bordure.getLargeur()));
-                        
-                        int x1 = i == 1 ? (int)rectangle.getMaxX() : (int)rectangle.getMinX();
-                        int y1 = i == 2 ? (int)rectangle.getMaxY() : (int)rectangle.getMinY();
-                        int x2 = i % 2 == 1 ? x1 : (int)rectangle.getMaxX();
-                        int y2 = i % 2 == 0 ? y1 : (int)rectangle.getMaxY();
-                        
-                        g2.drawLine(x1, y1, x2, y2);
-                    }
+                    dessinerRectangle(touche, g2);
                     break;
                     
                 // Triangle, pentagone, hexagone
                 default:
-                    // Dessin du polygone intérieur
-                    int nbBordures = Outils.nbBordures(forme);
-                    Polygon polygone = Outils.calculerPolygone(nbBordures, position, dimension);
-                    g2.setColor(apparence.getCouleurFond());
-                    g2.fillPolygon(polygone);
-                    
-                    // Dessin des bordures
-                    for (int i = 0; i < nbBordures; i++)
-                    {
-                        int x1, y1, x2, y2;
-                        
-                        x1 = polygone.xpoints[i];
-                        y1 = polygone.ypoints[i];
-                        
-                        if (i == nbBordures - 1)
-                        {
-                            x2 = polygone.xpoints[0];
-                            y2 = polygone.ypoints[0];
-                        }
-                        else
-                        {
-                            x2 = polygone.xpoints[i + 1];
-                            y2 = polygone.ypoints[i + 1];
-                        }
-                        
-                        bordure = apparence.getBordure(i);
-                        g2.setColor(bordure.getCouleur());
-                        g2.setStroke(new BasicStroke(bordure.getLargeur()));
-                        g2.drawLine(x1, y1, x2, y2);
-                    }
+                    dessinerPolygone(touche, g2);
                     break;
             }
             
-            // Dessin des bordures transversales
-            int premiereBordureTransversale = Outils.nbBordures(forme);
-            Bordure bordureHorizontale = apparence.getBordure(premiereBordureTransversale);
-            Bordure bordureVerticale = apparence.getBordure(premiereBordureTransversale + 1);
-            
-            if (bordureHorizontale != null)
+            dessinerBorduresTransversales(touche, g2);
+        }
+    }
+    
+    private void dessinerCercle(Touche touche, Graphics2D g2)
+    {
+        ApparenceTouche apparence = touche.getApparence();
+        Point2D position = Outils.conversionPointRelatifPixel(touche.getPosition(), dimensionPanneau);
+        Dimension2D dimension = Outils.conversionDimensionRelatifPixel(apparence.getDimension(), dimensionPanneau);
+        
+        // Dessin du cercle intérieur
+        g2.setColor(apparence.getCouleurFond());
+        int x = (int)position.getX() - (int)dimension.getWidth() / 2;
+        int y = (int)position.getY() - (int)dimension.getHeight() / 2;
+        g2.fillOval(x, y, (int)dimension.getWidth(), (int)dimension.getHeight());
+
+        // Dessin de la bordure
+        Bordure bordure = apparence.getBordure(0);
+        if (bordure != null)
+        {
+            g2.setColor(bordure.getCouleur());
+            g2.setStroke(new BasicStroke(bordure.getLargeur()));
+            g2.drawOval(x, y, (int)dimension.getWidth(), (int)dimension.getHeight());
+        }
+    }
+    
+    private void dessinerRectangle(Touche touche, Graphics2D g2)
+    {
+        ApparenceTouche apparence = touche.getApparence();
+        Point2D position = Outils.conversionPointRelatifPixel(touche.getPosition(), dimensionPanneau);
+        Dimension2D dimension = Outils.conversionDimensionRelatifPixel(apparence.getDimension(), dimensionPanneau);
+        
+        // Dessin du rectangle intérieur
+        g2.setColor(apparence.getCouleurFond());
+        int x = (int)position.getX() - (int)dimension.getWidth() / 2;
+        int y = (int)position.getY() - (int)dimension.getHeight() / 2;
+        Rectangle rectangle = new Rectangle(x, y, (int)dimension.getWidth(), (int)dimension.getHeight());
+        g2.fill(rectangle);
+
+        // Dessin des bordures
+        for (int i = 0; i < 4; i++)
+        {
+            Bordure bordure = apparence.getBordure(i);
+            g2.setColor(bordure.getCouleur());
+            g2.setStroke(new BasicStroke(bordure.getLargeur()));
+
+            int x1 = i == 1 ? (int)rectangle.getMaxX() : (int)rectangle.getMinX();
+            int y1 = i == 2 ? (int)rectangle.getMaxY() : (int)rectangle.getMinY();
+            int x2 = i % 2 == 1 ? x1 : (int)rectangle.getMaxX();
+            int y2 = i % 2 == 0 ? y1 : (int)rectangle.getMaxY();
+
+            g2.drawLine(x1, y1, x2, y2);
+        }
+    }
+    
+    private void dessinerPolygone(Touche touche, Graphics2D g2)
+    {
+        ApparenceTouche apparence = touche.getApparence();
+        Point2D position = Outils.conversionPointRelatifPixel(touche.getPosition(), dimensionPanneau);
+        Dimension2D dimension = Outils.conversionDimensionRelatifPixel(apparence.getDimension(), dimensionPanneau);
+        
+        // Dessin du polygone intérieur
+        int nbBordures = Outils.nbBordures(apparence.getForme());
+        Polygon polygone = Outils.calculerPolygone(nbBordures, position, dimension);
+        g2.setColor(apparence.getCouleurFond());
+        g2.fillPolygon(polygone);
+
+        // Dessin des bordures
+        for (int i = 0; i < nbBordures; i++)
+        {
+            int x1, y1, x2, y2;
+
+            x1 = polygone.xpoints[i];
+            y1 = polygone.ypoints[i];
+
+            if (i == nbBordures - 1)
             {
-                g2.setColor(bordureHorizontale.getCouleur());
-                g2.setStroke(new BasicStroke(bordureHorizontale.getLargeur()));
-                int x1 = (int)position.getX() - (int)dimension.getWidth() / 2;
-                int x2 = (int)position.getX() + (int)dimension.getWidth() / 2;
-                y = (int)position.getY();
-                g2.drawLine(x1, y, x2, y);
+                x2 = polygone.xpoints[0];
+                y2 = polygone.ypoints[0];
             }
-            
-            if (bordureVerticale != null)
+            else
             {
-                g2.setColor(bordureVerticale.getCouleur());
-                g2.setStroke(new BasicStroke(bordureVerticale.getLargeur()));
-                int y1 = (int)position.getY() - (int)dimension.getHeight() / 2;
-                int y2 = (int)position.getY() + (int)dimension.getHeight() / 2;
-                x = (int)position.getX();
-                g2.drawLine(x, y1, x, y2);
+                x2 = polygone.xpoints[i + 1];
+                y2 = polygone.ypoints[i + 1];
             }
+
+            Bordure bordure = apparence.getBordure(i);
+            g2.setColor(bordure.getCouleur());
+            g2.setStroke(new BasicStroke(bordure.getLargeur()));
+            g2.drawLine(x1, y1, x2, y2);
+        }
+    }
+    
+    private void dessinerBorduresTransversales(Touche touche, Graphics2D g2)
+    {
+        ApparenceTouche apparence = touche.getApparence();
+        Point2D position = Outils.conversionPointRelatifPixel(touche.getPosition(), dimensionPanneau);
+        Dimension2D dimension = Outils.conversionDimensionRelatifPixel(apparence.getDimension(), dimensionPanneau);
+        
+        int premiereBordureTransversale = Outils.nbBordures(apparence.getForme());
+        Bordure bordureHorizontale = apparence.getBordure(premiereBordureTransversale);
+        Bordure bordureVerticale = apparence.getBordure(premiereBordureTransversale + 1);
+
+        if (bordureHorizontale != null)
+        {
+            g2.setColor(bordureHorizontale.getCouleur());
+            g2.setStroke(new BasicStroke(bordureHorizontale.getLargeur()));
+            int x1 = (int)position.getX() - (int)dimension.getWidth() / 2;
+            int x2 = (int)position.getX() + (int)dimension.getWidth() / 2;
+            int y = (int)position.getY();
+            g2.drawLine(x1, y, x2, y);
+        }
+
+        if (bordureVerticale != null)
+        {
+            g2.setColor(bordureVerticale.getCouleur());
+            g2.setStroke(new BasicStroke(bordureVerticale.getLargeur()));
+            int y1 = (int)position.getY() - (int)dimension.getHeight() / 2;
+            int y2 = (int)position.getY() + (int)dimension.getHeight() / 2;
+            int x = (int)position.getX();
+            g2.drawLine(x, y1, x, y2);
         }
     }
 }
