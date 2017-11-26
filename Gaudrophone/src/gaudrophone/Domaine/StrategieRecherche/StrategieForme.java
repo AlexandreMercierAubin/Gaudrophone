1f8f1778cf0a025490d27215f563ef77cf8e7677
AlexandreMercierAubin <alexandremercieraubin@gmail.com>
2017-11-26 17:48:35 -0500

    issue fix reajout des strategies

--- Gaudrophone/src/gaudrophone/Domaine/StrategieRecherche/StrategieForme.java
+++ Gaudrophone/src/gaudrophone/Domaine/StrategieRecherche/StrategieForme.java
@@ -3,8 +3,9 @@ package gaudrophone.Domaine.StrategieRecherche;
 
 import gaudrophone.Domaine.Instrument.ApparenceTouche;
 import gaudrophone.Domaine.Instrument.Touche;
+import java.io.Serializable;
 
-public class StrategieForme extends StrategieRecherche{
+public class StrategieForme extends StrategieRecherche implements Serializable{
     @Override
     public boolean comparer(Touche touche, String mots)
     {
