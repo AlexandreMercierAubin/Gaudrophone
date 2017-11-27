4c096bab74f91845b5be61cc451598642206ac6f
VenablesA <VenablesA@VenablesA>
2017-11-27 11:25:25 -0500

    Enlever commentaire useless

--- Gaudrophone/src/gaudrophone/Domaine/Instrument/Note.java
+++ Gaudrophone/src/gaudrophone/Domaine/Instrument/Note.java
@@ -66,34 +66,6 @@ public class Note extends Son implements Serializable{
         {
             e.printStackTrace();
         }
-//        try {
-//
-//            Sequencer sequencer = MidiSystem.getSequencer();
-//            sequencer.open();
-//            Sequence sequence = new Sequence(Sequence.PPQ,4);
-//            Track track = sequence.createTrack();
-//
-//            MidiEvent event = null;
-//
-//            ShortMessage first = new ShortMessage();
-//            first.setMessage(192,1,0,0);
-//            MidiEvent changeInstrument = new MidiEvent(first, 0);
-//            track.add(changeInstrument);
-//
-//            ShortMessage a = new ShortMessage();
-//            a.setMessage(144,1,60,100);
-//            MidiEvent noteOn = new MidiEvent(a, 0);
-//            track.add(noteOn);
-//
-//            ShortMessage b = new ShortMessage();
-//            b.setMessage(128,1,60,100);
-//            MidiEvent noteOff = new MidiEvent(b, 9900);
-//            track.add(noteOff);
-//
-//            sequencer.setSequence(sequence);
-//            sequencer.start();
-//        } catch (Exception ex) { ex.printStackTrace(); }
-        //Ajouter code commencerJouer
     }
     
     @Override
