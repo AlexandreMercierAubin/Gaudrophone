9960820585bd5278cd1c74a173bdef0c8294999e
Samuel Baillargeon <samuel.baillargeon.1@ulaval.ca>
2017-12-12 15:08:36 -0500

    Clavier marche presque

--- Gaudrophone/src/gaudrophone/Domaine/Action/ActionArreterJouerTouche.java
+++ Gaudrophone/src/gaudrophone/Domaine/Action/ActionArreterJouerTouche.java
@@ -1,18 +1,27 @@
 package gaudrophone.Domaine.Action;
 
 import gaudrophone.Domaine.Instrument.Touche;
+import gaudrophone.Presentation.PanneauAffichage;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 
 public class ActionArreterJouerTouche extends AbstractAction {
     Touche touche;
+    PanneauAffichage panneauAffichage;
         
-    public ActionArreterJouerTouche(Touche touche){
+    public ActionArreterJouerTouche(Touche touche, PanneauAffichage panneau){
         this.touche = touche;
+        this.panneauAffichage = panneau;
     }
 
     @Override
     public void actionPerformed(ActionEvent e){
-        touche.arreterJouer();
+        if (touche.getCleAppuyee())
+        {
+            touche.arreterJouer();
+            touche.setSurbrillance(false);
+            touche.setCleAppuyee(false);
+            panneauAffichage.repaint();
+        }
     }
 }
--- Gaudrophone/src/gaudrophone/Domaine/Action/ActionCommencerJouerTouche.java
+++ Gaudrophone/src/gaudrophone/Domaine/Action/ActionCommencerJouerTouche.java
@@ -1,18 +1,29 @@
 package gaudrophone.Domaine.Action;
 
 import gaudrophone.Domaine.Instrument.Touche;
+import gaudrophone.Presentation.PanneauAffichage;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 
 public class ActionCommencerJouerTouche extends AbstractAction {
     Touche touche;
-        
-    public ActionCommencerJouerTouche(Touche touche){
+    PanneauAffichage panneauAffichage;
+    
+    public ActionCommencerJouerTouche(Touche touche, PanneauAffichage panneau)
+    {
         this.touche = touche;
+        panneauAffichage = panneau;
     }
-
+    
     @Override
-    public void actionPerformed(ActionEvent e){
-        touche.commencerJouer();
+    public void actionPerformed(ActionEvent e)
+    {
+        if (!touche.getCleAppuyee())
+        {
+            touche.commencerJouer();
+            touche.setSurbrillance(true);
+            touche.setCleAppuyee(true);
+            panneauAffichage.repaint();
+        }
     }
 }
